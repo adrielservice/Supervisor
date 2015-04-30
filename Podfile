90cb0925e3c0ccc8ed3c695aa7ee7efe221022c0
David Beilis <david.beilis@genesys.com>
2015-04-30 00:16:22 -0700

    DB - Added watch kit

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+pod 'PNChart'
+pod 'RestKit', '~> 0.20.0'
---
+++ Supervisor WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Supervisor WatchKit Extension
+//
+//  Created by David Beilis on 4/30/15.
+//  Copyright (c) 2015 Genesys. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
